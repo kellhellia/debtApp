59aae89b2bfd7e45d6ea50be10a7b04282c49944
kellhellia <kellhellia@gmail.com>
2015-11-26 18:13:12 +0200

    all works fine

--- main.js
+++ main.js
@@ -4,65 +4,88 @@ class App extends React.Component {
         this.state = {
             myCredits: [],
             guysCredits: [],
-            totalGuysCredit: null
+            totalGuysCredit: null,
+            alert: false
         };
     }
 
     componentDidMount() {
         let ls = localStorage;
-
-        let arr = [];
-        let lsList = JSON.parse(ls.getItem("list"));
-
-        this.setState({guysCredits: lsList});
-
-        if (!lsList) {
-            ls.setItem("list", JSON.stringify(arr));
-        }
-
+        let guysCredits = JSON.parse(ls.getItem("list"));
         let sumArr = [];
 
-        _.each(lsList, (n, index) => {
-            sumArr.push(+n.credit);
-        });
+        if (!guysCredits || guysCredits.length === 0) {
+            let arr = [];
+            sumArr = ["0"];
+            ls.setItem("list", JSON.stringify(arr));
+            ls.setItem("totalGuysCredit", JSON.stringify(sumArr));
 
-        let totalGuysCredit = _.reduce(sumArr, (total, n) => {
-            return total + n;
-        });
+            this.setState({totalGuysCredit: sumArr});
+        } else if (guysCredits) {
+            _.each(guysCredits, (n, index) => {
+                sumArr.push(+n.credit);
+            });
 
-        this.setState({totalGuysCredit});
-        console.log(totalGuysCredit);
-        console.log('state', this.state.totalGuysCredit);
+            let totalGuysCredit = _.reduce(sumArr, (total, n) => {
+                return total + n;
+            });
+
+            this.setState({guysCredits, totalGuysCredit}, () => {
+                console.log(this.state);
+            });
+        }
+    }
+
+    handleOnFocusInput(e) {
+        this.setState({alert: false});
     }
 
     handleAddCredit(e) {
         e.preventDefault();
+
+        //get data from localStorage and inputs
+
         let ls = localStorage;
 
         let list = JSON.parse(ls.getItem("list"));
 
         let name = this.refs.name.value;
+        let description = this.refs.description.value;
         let credit = this.refs.credit.value;
 
-        let totalGuysCredit = +credit + +this.state.totalGuysCredit;
-        console.log(totalGuysCredit);
+        // calculate value of total credit
 
-        this.setState({totalGuysCredit});
+        let numberExpr = /^[0-9]*$/g;
 
-        let el = {
-            name,
-            credit
+        if (((credit.toString())).match(numberExpr)) {
+            let totalGuysCredit = +credit + +this.state.totalGuysCredit;
+
+            this.setState({totalGuysCredit});
+
+            //set item to localStorage and state
+
+            let el = {
+                name,
+                description,
+                credit
+            }
+
+            list.push(el);
+
+            ls.setItem("list", JSON.stringify(list));
+            let guysCredits = this.state.guysCredits;
+            guysCredits.push(el);
+            this.setState({guysCredits});
+
+            // clear our inputs
+
+            this.refs.credit.value = null;
+            this.refs.name.value = null;
+        } else {
+            // set alert
+
+            this.setState({alert});
         }
-
-        list.push(el);
-
-        ls.setItem("list", JSON.stringify(list));
-        let guysCredits = this.state.guysCredits;
-        guysCredits.push(el);
-        this.setState({guysCredits});
-
-        this.refs.credit.value = null;
-        this.refs.name.value = null;
     }
 
     handleDelete(user) {
@@ -86,21 +109,28 @@ class App extends React.Component {
 
         let guysList = _.map(this.state.guysCredits, (n, index) => {
             return <li className="clearfix" key={index}>
-                      <span className="list__item pull-left">
-                        <span className="pull-left">{n.name}</span>
-                        <span className="pull-right">
-                            {n.credit}
-                        </span>
-                      </span>
+                      <div className="list__item">
+                          <div className="row">
+                             <div className="col-xs-6">
+                                {n.name}
+                             </div>
 
-                      <span className="pull-right">
-                        <i className="list__icon blue glyphicon glyphicon-pencil"></i>
-                        <i onClick={this.handleDelete.bind(this, n)} className="list__icon red glyphicon glyphicon-remove"></i>
-                      </span>
+                             <div className="col-xs-6 text-right">
+                                <i className="list__icon blue glyphicon glyphicon-pencil"></i>
+                                <i onClick={this.handleDelete.bind(this, n)} className="list__icon red glyphicon glyphicon-remove"></i>
+                              </div>
+                          </div>
+                          <div className="row">
+                             <div className="col-xs-12">
+                                {n.credit} - {n.description}
+                             </div>
+                          </div>
+                      </div>
                    </li>;
         });
 
         let list = (this.state.guysCredits.length !== 0) ? '' : "Please, add new debt :)";
+        let alert = this.state.alert ? <div className="alert alert-danger" role="alert">Please, enter number in dollar input</div> : "";
 
         return (
             <div className="container">
@@ -116,15 +146,41 @@ class App extends React.Component {
                                       <span className="input-group-addon">
                                         <i className="glyphicon glyphicon-user" />
                                       </span>
-                                      <input ref="name" className="form-control form-group" required />
+                                      <input
+                                            ref="name"
+                                            onFocus={this.handleOnFocusInput.bind(this)}
+                                            className="form-control form-group"
+                                            placeholder="Name"
+                                            required
+                                      />
+                                    </div>
+
+                                    <div className="input-group form-group">
+                                      <span className="input-group-addon">
+                                        <i className="glyphicon glyphicon-pencil" />
+                                      </span>
+                                      <input
+                                            ref="description"
+                                            onFocus={this.handleOnFocusInput.bind(this)}
+                                            className="form-control"
+                                            placeholder="Description(optional)"
+                                      />
                                     </div>
 
                                     <div className="input-group form-group">
                                       <span className="input-group-addon">
                                         <i className="glyphicon glyphicon-usd" />
                                       </span>
-                                      <input ref="credit" className="form-control" required />
+                                      <input
+                                          ref="credit"
+                                          onFocus={this.handleOnFocusInput.bind(this)}
+                                          className="form-control"
+                                          placeholder="Debt"
+                                          required
+                                      />
                                     </div>
+
+                                    {alert}
                                 </div>
                             </div>
 
